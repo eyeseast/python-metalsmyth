1282590ed8cf04a8e1923b139f3ba64b357fc25d
chrisamico <eyeseast@gmail.com>
2014-11-11 11:32:37 -0500

    Added build method. This needs tests.

--- metalsmyth/__init__.py
+++ metalsmyth/__init__.py
@@ -25,16 +25,17 @@ class Stack(object):
         return a dictionary of path => post
         """
         files = {}
-        for filename in os.listdir(self.src):
-            path = os.path.join(self.src, filename)
+        for filename in os.listdir(self.source):
+            path = os.path.join(self.source, filename)
             files[filename] = frontmatter.load(path)
 
         return files
 
-    def run(self):
+    def run(self, files=None):
         "Run each middleware function on files"
-        # load files from source directory
-        files = self.get_files()
+        # use provided files, or load files from source directory
+        if files is None:
+            files = self.get_files()
 
         # loop through each middleware
         for func in self.middleware:
@@ -46,6 +47,24 @@ class Stack(object):
         self.files = files
         return files
 
-    
+    def build(self):
+        "Build out results to dest directory (creating if needed)"
+
+        # ensure a build dir
+        if not os.path.isdir(self.dest):
+            os.makedirs(self.dest)
+
+        # make sure we have files
+        files = getattr(self, 'files', self.run())
+
+        # write the content of each post to dest, using keys as filenames
+        for filename, post in self.files.iteritems():
+
+            # join filename to dest dir
+            path = os.path.join(self.dest, filename)
+            with open(path, 'w') as f:
+                f.write(post.content.encode('utf-8'))
+
+
 
 
